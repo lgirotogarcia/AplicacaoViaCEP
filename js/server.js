f43be9a655cbe690eb1581fa41154b22d7909e54
Leonardo Garcia <leonardogirotogarcia@gmail.com>
2021-09-10 19:15:32 -0300

    Pequenas alteraçoes visuais, sem impacto no funcionamento do codigo

--- js/server.js
+++ js/server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const db = require('../db.js'); /*conectar no banco de dados*/
-
-
+const db = require('../db.js');
 
 app.get('/:cep', async (req, res) => {
     try {
         const data = await axios.get(`http://viacep.com.br/ws/${req.params.cep}/json`)
         const viaCepResponse = data.data;
         const Cep = db.Mongoose.model('cep', db.CepSchema);
-        const docs = await Cep.findOne({cep: viaCepResponse.cep},{_id: 0, __v: 0, createdAt: 0, updatedAt: 0})
+        const docs = await Cep.findOne({cep: viaCepResponse.cep},{_id: 0, __v: 0, createdAt: 0, updatedAt: 0});
     
         if(!docs){
             const newDoc = new Cep(viaCepResponse);
@@ -22,10 +20,10 @@ app.get('/:cep', async (req, res) => {
         }
         return res.json(docs);  
     } catch (error) {
-        return res.send('CEP não existente.')
+        return res.send('CEP não existente.');
     }
-})
+});
 
 app.listen(3000, () => {
     console.log('Server on')
-})
+});
